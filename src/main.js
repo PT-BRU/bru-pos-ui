b98c04e1b1aaa2eec3034db77596eca04a08f609
achensetyawati <achenzetya@gmail.com>
2022-10-17 10:34:01 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -18,10 +18,10 @@ export async function configure(aurelia) {
     .standardConfiguration()
     .feature("components")
     .plugin("aurelia-api", (config) => {
-      var auth = "https://com-bru-service-auth-dev.azurewebsites.net/v1/";
-      var core = "https://com-bru-service-core-dev.azurewebsites.net/v1/";
-      var pos = "https://com-bru-service-pos-dev.azurewebsites.net/v1/";
-      var inventory = "https://com-bru-service-warehouse-dev.azurewebsites.net/v1/";
+      var auth = "https://com-bru-service-auth.azurewebsites.net/v1/";
+      var core = "https://com-bru-service-core.azurewebsites.net/v1/";
+      var pos = "https://com-bru-service-pos.azurewebsites.net/v1/";
+      var inventory = "https://com-bru-service-warehouse.azurewebsites.net/v1/";
 
       config.registerEndpoint("auth", auth);
       config.registerEndpoint("core", core);
